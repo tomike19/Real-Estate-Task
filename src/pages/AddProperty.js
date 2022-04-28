a4bb4b5ce2a6e6c2709d4a82b870b9283d0983ee
tomike19 <kolajoelizabeth@gmail.com>
2022-04-28 13:33:34 -0700

    add property list page fixed

--- src/pages/AddProperty.js
+++ src/pages/AddProperty.js
@@ -21,44 +21,87 @@ const AddProperty = () => {
               />
             </div>
             <div className="col-md-6 col-lg-6 col-12">
-              <Input
-                label="Property Type"
-                placeholder="Enter your text"
-                type="text"
-                name="property-type"
-              />
+              <div className="input-wrap d-flex flex-column">
+                <label className="input-label">Property type</label>
+                <select
+                  class="form-select input"
+                  name="property-type"
+                  type="text"
+                  aria-label="Default                                                      select example"
+                >
+                  <option selected>Property Type</option>
+                  <option>Duplex</option>
+                  <option>Bungalow</option>
+                </select>
+              </div>
             </div>
             <div className="col-md-6 col-lg-6 col-12">
-              <Input
-                label="Number of Bathrooms"
-                placeholder="Enter your text"
-                type="text"
-                name="Number of Bathrooms"
-              />
+              <div className="input-wrap d-flex flex-column">
+                <label className="input-label">Number of Bathrooms</label>
+                <select
+                  class="form-select input"
+                  name="property-type"
+                  type="text"
+                  aria-label="Default                                                      select example"
+                >
+                  <option selected> Number of Bathrooms</option>
+                  <option>Bathrooms 1</option>
+                  <option>Bathrooms 2</option>
+                  <option>Bathrooms 3</option>
+                  <option>Bathrooms 4</option>
+                </select>
+              </div>
             </div>
             <div className="col-md-6 col-lg-6 col-12">
-              <Input
-                label="Enter your text"
-                placeholder="Enter your text"
-                type="text"
-                name="text"
-              />
+              <div className="input-wrap d-flex flex-column">
+                <label className="input-label">Number of Kitchens</label>
+                <select
+                  class="form-select input"
+                  name="property-type"
+                  type="text"
+                  aria-label="Default                                                      select example"
+                >
+                  <option selected> Number of Kitchens</option>
+                  <option>Kitchen 1</option>
+                  <option>Kitchen 2</option>
+                  <option>Kitchen 3</option>
+                  <option>Kitchen 4</option>
+                </select>
+              </div>
             </div>
             <div className="col-md-6 col-lg-6 col-12">
-              <Input
-                label="Number of Toilets"
-                placeholder="Enter your text"
-                type="text"
-                name="Number of Toilets"
-              />
+              <div className="input-wrap d-flex flex-column">
+                <label className="input-label">Number of Toilets</label>
+                <select
+                  class="form-select input"
+                  name="property-type"
+                  type="text"
+                  aria-label="Default                                                      select example"
+                >
+                  <option selected> Number ofToilets</option>
+                  <option>Toilets 1</option>
+                  <option>Toilets 2</option>
+                  <option>Toilets 3</option>
+                  <option>Toilets 4</option>
+                </select>
+              </div>
             </div>
             <div className="col-md-6 col-lg-6 col-12">
-              <Input
-                label="Enter your text"
-                placeholder="Enter your text"
-                type="text"
-                name="text"
-              />
+              <div className="input-wrap d-flex flex-column">
+                <label className="input-label">Number of SittingRooms</label>
+                <select
+                  class="form-select input"
+                  name="property-type"
+                  type="text"
+                  aria-label="Default                                                      select example"
+                >
+                  <option selected> Number of SittingRooms</option>
+                  <option>SittingRooms 1</option>
+                  <option>SittingRooms 2</option>
+                  <option>SittingRooms 3</option>
+                  <option>SittingRooms 4</option>
+                </select>
+              </div>
             </div>
             <div className="col-md-6 col-lg-6 col-12">
               <Input
@@ -83,6 +126,37 @@ const AddProperty = () => {
                 name="valid to"
                 placeholder="dd-mm-yyyy"
               />
+            </div>
+            <div class="col-lg-12 col-12 mt-5">
+              <div class="form-floating">
+                <textarea
+                  name="description"
+                  class="form-control
+                    mb-5"
+                  type="text"
+                  placeholder="Leave a
+                   comment here"
+                  id="floatingTextarea"
+                ></textarea>
+                <label for="floatingTextarea">Description</label>
+              </div>
+            </div>
+            <div
+              class="add-property-box
+              step-3 input-file"
+            >
+              <div class="input-file">
+                <label for="" className="input-label">
+                  Upload Property Images
+                </label>
+                <input
+                  class="form-control
+                  form-control-lg"
+                  id="formFileLg"
+                  type="file"
+                  name="image"
+                />
+              </div>
             </div>
           </div>
           <div className="add-property__footer">
