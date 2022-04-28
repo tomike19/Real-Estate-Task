c8fc02ed60c5e8639de8ca5978f89cfe1e3c56a6
tomike19 <kolajoelizabeth@gmail.com>
2022-04-28 15:43:37 -0700

    show property list form fixed

--- src/pages/ListProperty.js
+++ src/pages/ListProperty.js
@@ -13,24 +13,24 @@ const ListPropertyPage = () => {
           <div className="list-property__property-form">
             <div className="row">
               <div className="col-md-6 col-lg-6 col-12">
-                  <div class="row mt-5">
-        <div class="col-md-5">
-            <div class="">rounded search input with icon</div>
-            <div class="input-group">
-                <input class="form-control border-end-0 border" type="search" value="search" id="example-search-input"/>
-                <span class="input-group-append">
-                    <button class="btn btn-outline-secondary bg-white border-start-0 border-bottom-0 border ms-n5" type="button">
-                        <i class="fa fa-search"></i>
-                    </button>
-                </span>
-            </div>
-        </div>
-    </div>
-                {/* <Input
-                  placeholder="Enter your address"
-                  type="text"
-                  name="Property-address"
-                /> */}
+                <div class="row mt-5">
+                  <div class="input-wrap input-group">
+                    <input
+                      class="form-control  search-input input"
+                      type="search"
+                      value="Find a property by address"
+                      id="example-search-input"
+                    />
+                    <span class="input-group-append">
+                      <button
+                        class="btn btn-outline- list-property__button bg-white ms-n5 mt-2"
+                        type="button"
+                      >
+                        <i class="bi bi-search pt-3"></i>
+                      </button>
+                    </span>
+                  </div>
+                </div>
               </div>
               <div className="col-md-6 col-lg-6 col-12 mt-5">
                 <div className="input-wrap d-flex flex-column filter-select">
